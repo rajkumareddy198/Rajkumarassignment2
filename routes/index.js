2541c7b61af5c4baf29a492ba2183e94e6bb606f
Raja reddy <rajkumarreddybanala@gmail.com>
2022-02-05 23:33:57 -0500

    changes made

--- routes/index.js
+++ routes/index.js
@@ -5,20 +5,21 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home' });
 });
+
 router.get('/home', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+  res.render('index', { title: 'Welcome to my portfolio site' });
 });
 router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About' });
+  res.render('index', { title: 'Im raj kumar Reddy, I am full time student at centennial college.I was an software engineer in india. I provide services of software development tutorials'});
 });
 router.get('/services', function(req, res, next) {
-  res.render('index', { title: 'Services' });
+  res.render('index', { title: 'I provide tutorial classes for cloud development' });
 });
 router.get('/projects', function(req, res, next) {
-  res.render('index', { title: 'Projects' });
+  res.render('index', { title: ' The projects that i have done are the automation of data renedering for axa-insurance' });
 });
 router.get('/contact', function(req, res, next) {
-  res.render('index', { title: 'Contact' });
+  res.render('index', { title: 'Email:rajkumarreddybanala@gmail.com Mobile no:4372387857' });
 });
 
 module.exports = router;
